a337a99d91831fdb4295b193f20a1ed0104c67fb
Katie Caserta <katiecaserta@gmail.com>
2022-08-01 17:41:45 +0100

    connect to mongoDB database

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3333;
+
+//connect to MongoDB
+const dbURI = `mongodb+srv://katiecaserta:Lighthouse333@lighthouse1.h4zhazg.mongodb.net/LighthouseDB?retryWrites=true&w=majority`;
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    //listen to port, only once connected successfully to DB
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT} 🚀`);
+    });
+    console.log("connected to db");
+  })
+  .catch((err) => console.log(err));
+
+//second argument above with object may not be necessary but if get deprecation errors, this is what resolves
 
 //middleware
 app.use(cors());
@@ -12,9 +29,3 @@ const servicesRouter = require("./routes/services.js");
 
 //set up the routers
 app.use("/services", servicesRouter);
-
-//listen to port
-const PORT = process.env.PORT || 3333;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} 🚀`);
-});
