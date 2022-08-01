f049db5a942fb7b59cecc83dc4be6c12bad73879
Katie Caserta <katiecaserta@gmail.com>
2022-08-01 18:34:19 +0100

    create models and schemas for services and reviews

---
+++ models/review.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    dateCreated: { type: Date, default: Date.now },
+    review: {
+      type: String,
+      required: true,
+    },
+    rating: Number,
+    serviceId: String,
+    dateReferred: Date,
+    dateAccessed: Date,
+  },
+  { timestamps: true }
+);
+
+const Review = mongoose.model("Review", reviewSchema);
+
+module.exports = Review;
--- models/service.js
+++ models/service.js
@@ -1,2 +1,40 @@
 const mongoose = require("mongoose");
-const Scheme = mongoose.Schema;
+const Schema = mongoose.Schema;
+
+const serviceSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    dateCreated: { type: Date, default: Date.now },
+    description: {
+      type: String,
+      required: true,
+    },
+    ratings: Array,
+    overallRating: Number,
+    summary: {
+      type: String,
+      required: true,
+    },
+    waitingTimeMonths: {
+      type: Number,
+      required: true,
+    },
+    lgbtq: {
+      type: Boolean,
+      required: true,
+    },
+    accessible: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Service = mongoose.model("Service", serviceSchema);
+
+module.exports = Service;
